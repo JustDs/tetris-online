bdde61295d4c8e8048b223643bb8bbcfc4df9070
Ticshot <944161746@qq.com>
2013-07-05 16:57:35 +0800

    finish painting background, painting blocks (static)

--- src/Painter.h
+++ src/Painter.h
@@ -22,11 +22,21 @@ public:
 	void init();
 	void paintOnline();
 	void setData(const TetrisData *self_data, const TetrisData *other_data);
+	void resizeWindow(int width,int height);
+
 private:
 	const TetrisData *self_data, *other_data;
-	int &window_width, &window_height;
+	int window_width, window_height;
 	func4i fillRect;	
 	func4i setColor;
+	unsigned int loadImage(char* filename);//load image from the resource
+	void paintImage(int x,int y, int width, int height, unsigned int img_id);//paint a picture
+
+	unsigned int block_img_id_list[7];
+	unsigned int background_img_id; 
+	unsigned int getImageId(char block_type);
+	void paintBackground();
+	void paintBlock(int x,int y,unsigned int block_img_id);
+	void paintSingle(int offset_x,int offset_y);
 };
 #endif
-
